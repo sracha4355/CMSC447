11242d6ac931dc3ed203d48de74f56a48beb177e
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-03 00:07:46 -0400

    Update single.sql

--- database/mysql/single.sql
+++ database/mysql/single.sql
@@ -1,17 +1,4 @@
 CREATE TABLE IF NOT EXISTS `single` (
-<<<<<<< Updated upstream
-    `single_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `single_name` VARCHAR(255) NOT NULL,
-    `single_length` VARCHAR(8) DEFAULT NULL,
-    `single_cover` VARCHAR(255) DEFAULT NULL,
-    `artist_id` INT UNSIGNED DEFAULT NULL,
-    `single_boomscore` INT NOT NULL,
-    PRIMARY KEY (`single_id`),
-    UNIQUE KEY `single_cover` (`single_cover`),
-    KEY `artist_id` (`artist_id`),
-    CONSTRAINT `artist_id` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`) ON DELETE SET NULL
-)
-=======
   `single_id` int unsigned NOT NULL AUTO_INCREMENT,
   `single_name` varchar(255) NOT NULL,
   `single_cover` varchar(255) DEFAULT NULL,
@@ -24,5 +11,5 @@ CREATE TABLE IF NOT EXISTS `single` (
   `dislikes` int unsigned DEFAULT '0',
    PRIMARY KEY (`single_id`),
    UNIQUE KEY `spotify_uid` (`spotify_uid`)
-);
->>>>>>> Stashed changes
+)
+
