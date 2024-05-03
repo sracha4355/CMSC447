1c5a53bc14353ce25555b4e0d90c4a47709c465c
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-03 00:14:25 -0400

    Update review.sql

--- database/mysql/review.sql
+++ database/mysql/review.sql
@@ -1,18 +1,4 @@
 CREATE TABLE IF NOT EXISTS `review` (
-<<<<<<< Updated upstream
-    `review_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `account_id` INT UNSIGNED NOT NULL,
-    `music_id` INT UNSIGNED NOT NULL,
-    `like_count` INT NOT NULL,
-    `dislike_count` INT NOT NULL,
-    `creation_date` DATE NOT NULL,
-    `review` TEXT NOT NULL,
-    PRIMARY KEY (`review_id`),
-    KEY `account_id` (`account_id`),
-    KEY `music_id` (`music_id`),
-    CONSTRAINT `review_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `acct` (`account_id`),
-    CONSTRAINT `review_ibfk_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
-=======
   `review_id` int unsigned NOT NULL AUTO_INCREMENT,
   `account_id` int unsigned NOT NULL,
   `music_id` int unsigned NOT NULL,
@@ -25,5 +11,4 @@ CREATE TABLE IF NOT EXISTS `review` (
   KEY `music_id` (`music_id`),
   CONSTRAINT `review_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `acct` (`account_id`),
   CONSTRAINT `review_ibfk_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
->>>>>>> Stashed changes
-)
+);
