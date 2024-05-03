16650118607832fde93073913e8917d1a2d6a01f
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-02 23:35:38 -0400

    Update SlideWithImageArtist.js

--- frontend/BoomBox/boombox/src/webpages/SlideWithImageArtist.js
+++ frontend/BoomBox/boombox/src/webpages/SlideWithImageArtist.js
@@ -1,18 +1,12 @@
 import React from "react";
-<<<<<<< Updated upstream
-=======
 import './SlideWithImageArtist.css'
->>>>>>> Stashed changes
 
 const SlideWithImageArtist = ({ album , imageURL}) => {
     return (
         <div className="slide-with-image">
             <div className="image-container">
-<<<<<<< Updated upstream
                 <img src= {imageURL} alt="genre_image" style={{width : '125px', height: '125px', borderRadius:'20%'}} />
-=======
                 <img src= {imageURL} alt="genre_image" style={{width : '125px', height: '125px', borderRadius:'20%'}} id="album-image" />
->>>>>>> Stashed changes
             </div>
             <div className="genre-name" style={{fontFamily: 'Bungee, sans-serif'}}>{album}</div>
         </div>
