7972fbc78aeddfae6f1a2173fd6f1999754d9ac0
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-03 00:11:35 -0400

    Update artist.sql

--- database/mysql/artist.sql
+++ database/mysql/artist.sql
@@ -1,12 +1,4 @@
 CREATE TABLE IF NOT EXISTS `artist` (
-<<<<<<< Updated upstream
-    `artist_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `artist_name` VARCHAR(255) NOT NULL,
-    `artist_picture` VARCHAR(255) DEFAULT NULL,
-    `artist_boomscore` INT NOT NULL,
-    PRIMARY KEY (`artist_id`, `artist_name`)
-)
-=======
   `artist_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `artist_name` VARCHAR(255) NOT NULL,
   `artist_picture` VARCHAR(255) DEFAULT NULL,
@@ -18,4 +10,3 @@ CREATE TABLE IF NOT EXISTS `artist` (
   PRIMARY KEY (`artist_id`),
   UNIQUE KEY `spotify_uid` (`spotify_uid`)
 );
->>>>>>> Stashed changes
