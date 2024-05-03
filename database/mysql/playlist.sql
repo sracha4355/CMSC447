065a9dceda0c4925baf28464c8db224eac910d5b
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-03 00:15:34 -0400

    Update playlist.sql

--- database/mysql/playlist.sql
+++ database/mysql/playlist.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS `playlist` (
   PRIMARY KEY (`playlist_id`),
   KEY `account_id` (`account_id`),
   CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `acct` (`account_id`)
-) 
+);
 
