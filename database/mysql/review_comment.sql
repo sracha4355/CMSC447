5ec16b0e802c015f32058f1cb3f1632443c17960
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-03 00:14:44 -0400

    Update review_comment.sql

--- database/mysql/review_comment.sql
+++ database/mysql/review_comment.sql
@@ -1,16 +1,5 @@
 
 CREATE TABLE IF NOT EXISTS `review_comment` (
-<<<<<<< Updated upstream
-    `review_comment_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `review_id` INT UNSIGNED NOT NULL,
-    `comment` TEXT NOT NULL,
-    `like_count` INT NOT NULL,
-    `dislike_count` INT NOT NULL,
-    `creation_date` DATE NOT NULL,
-    PRIMARY KEY (`review_comment_id`, `review_id`),
-    CONSTRAINT `review_comment_ibfk_1` FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`)
-)
-=======
   `review_comment_id` int unsigned NOT NULL AUTO_INCREMENT,
   `review_id` int unsigned NOT NULL,
   `comment` text NOT NULL,
@@ -20,5 +9,5 @@ CREATE TABLE IF NOT EXISTS `review_comment` (
   PRIMARY KEY (`review_comment_id`,`review_id`),
   KEY `review_comment_ibfk_1` (`review_id`),
   CONSTRAINT `review_comment_ibfk_1` FOREIGN KEY (`review_id`) REFERENCES `review` (`review_id`)
-) 
->>>>>>> Stashed changes
+);
+
